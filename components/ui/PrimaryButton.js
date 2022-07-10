27976b681ff77137840a9de2029dfeb787988774
Juciano <juciano.junior@hotmail.com>
2022-07-10 16:43:16 +0100

    ui's components folder

---
+++ components/ui/PrimaryButton.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, Text, Pressable } from "react-native"
+
+import Color from '../../constants/colors'
+
+const PrimaryButton = ({ children, onPress }) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+        <Pressable
+          style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+          }
+          onPress={onPress}
+          android_ripple={{color: Color.primary600}}>
+          <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+      </View>
+  )
+}
+
+export default PrimaryButton
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: Color.primary500,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  }
+})
