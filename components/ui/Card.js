e17ba31308a622900edf53c6f46377b110af36f3
Juciano <juciano.junior@hotmail.com>
2022-07-12 00:03:34 +0100

    Card component

---
+++ components/ui/Card.js
@@ -0,0 +1,30 @@
+import { StyleSheet, View } from "react-native"
+
+import Color from "../../constants/colors"
+
+const Card = ({ children }) => {
+  return (
+    <View style={styles.card}>
+        {children}
+    </View>
+  )
+}
+
+export default Card
+
+const styles = StyleSheet.create({
+    card: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 36,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: Color.primary800,
+        borderRadius: 8,
+        elevation: 4, //shadows for android
+        shadowColor: 'black', // shadows for IOS 
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    },
+})
