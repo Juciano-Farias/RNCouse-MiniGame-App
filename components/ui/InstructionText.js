2c855fa95f3ac05d987374920716451d60b2f989
Juciano <juciano.junior@hotmail.com>
2022-07-12 00:03:54 +0100

    instruction text component

---
+++ components/ui/InstructionText.js
@@ -0,0 +1,18 @@
+import { Text, StyleSheet } from "react-native"
+
+import Color from "../../constants/colors"
+
+const InstructionText = ({ children, style }) => {
+  return (
+    <Text style={[styles.instructionText, style]}>{children}</Text>
+  )
+}
+
+export default InstructionText
+
+const styles = StyleSheet.create({
+    instructionText: {
+        color: Color.secondary500,
+        fontSize: 24,
+    },
+})
